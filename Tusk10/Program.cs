577a55f560741c151654c06ce18755b0070f8849
Shumakov F.P <Grodogad@gmail.com>
2023-04-03 12:09:58 +0700

    add Tusk 10

---
+++ Tusk10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трёхзначное число");
+int number = int.Parse(Console.ReadLine());
+
+if (number / 100 == 0 || number > 999)
+{
+    Console.WriteLine("Обманывать плохо!");
+}
+else
+{
+    int result = number % 100;
+    Console.Write(result / 10);
+}
+
