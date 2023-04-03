169398dc07da47a43139390778e6ca6745cbdf0d
Shumakov F.P <Grodogad@gmail.com>
2023-04-03 15:13:29 +0700

    add Tusk 13

---
+++ Tusk13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int array = number.ToString().Length;
+if (array >= 3) {
+	while (number > 999)
+	{
+		number = number / 10;
+	}
+	int result = number % 10;
+	Console.WriteLine("Третья цифра " + result);
+} 
+else 
+	Console.WriteLine("Третей цифры нет");
